237a028bd04f3cdad47a9e009c35e817b0cb0c6b
Avijadhav01 <avipatil9699403305@gmail.com>
2025-12-19 22:49:37 +0530

    feat: Add product and review slices with async actions for fetching and creating products and reviews

---
+++ Frontend/src/features/products/productSlice.js
@@ -0,0 +1,120 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/api/v1",
+  headers: {
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache",
+  },
+});
+
+export const getProducts = createAsyncThunk(
+  "products/all",
+  async (filter, { rejectWithValue }) => {
+    try {
+      // console.log(filter);
+      const { data } = await api.get("/products/get-all", {
+        params: {
+          page: filter?.page || 1,
+          limit: filter?.limit || 8,
+          keyword: filter?.keyword,
+          category: filter?.category,
+        },
+      });
+      // console.log("Backend Response:", data);
+      return data.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.message || error.message);
+    }
+  }
+);
+
+export const getProductDetails = createAsyncThunk(
+  "products/getProductDetails",
+  async (id, { rejectWithValue }) => {
+    try {
+      const { data } = await api.get(`/products/${id}`);
+      // console.log("hii: ", data);
+      return data.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.message || error.message);
+    }
+  }
+);
+
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    products: [],
+    productCount: 0,
+
+    currPage: 0,
+    isNextPage: false,
+    isPrevPage: false,
+    totalPages: 0,
+
+    loading: false,
+    error: null,
+    product: null,
+    searchKeyword: "",
+  },
+  reducers: {
+    removeErrors: (state) => {
+      state.error = null;
+    },
+    setSearchKeyword: (state, action) => {
+      // console.log("Searched : ", action.payload);
+      state.searchKeyword = action.payload;
+    },
+    unSetSearchKeyword: (state) => {
+      state.searchKeyword = "";
+    },
+  },
+
+  extraReducers: (builder) => {
+    // Get Products
+    builder
+      .addCase(getProducts.pending, (state, action) => {
+        state.products = []; // reset product list
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.products = action.payload.docs || [];
+        state.currPage = action.payload.page || 0;
+        state.isNextPage = action.payload.hasNextPage || false;
+        state.isPrevPage = action.payload.hasPrevPage || false;
+        state.totalPages = action.payload.totalPages || 0;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.loading = false;
+        // console.log("Rejected action payload: ", action.payload);
+        state.error = action.payload || "Error while getting products";
+      });
+
+    // get product
+    builder
+      .addCase(getProductDetails.pending, (state, action) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getProductDetails.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.product = action.payload;
+      })
+      .addCase(getProductDetails.rejected, (state, action) => {
+        console.log("Rejected action payload: ", action.payload);
+        state.loading = false;
+        state.error = action.payload || "Error while getting product details";
+        state.product = null;
+      });
+  },
+});
+
+export const { removeErrors, setSearchKeyword, unSetSearchKeyword } =
+  productSlice.actions;
+export default productSlice.reducer;
---
+++ Frontend/src/features/reviews/reviewSlice.js
@@ -0,0 +1,118 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/api/v1",
+  headers: {
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache",
+  },
+});
+
+export const getProductReviews = createAsyncThunk(
+  "reviews/getProductReviews",
+  async (formData, { rejectWithValue }) => {
+    try {
+      const { data } = await api.get(`/reviews/${formData.id}`, {
+        params: {
+          page: formData.page || 1,
+          limit: formData.limit || 4,
+        },
+      });
+      // console.log("hii: ", data);
+      return data.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.message || error.message);
+    }
+  }
+);
+
+export const createProductReview = createAsyncThunk(
+  "reviews/createProductReview",
+  async (formData, { rejectWithValue }) => {
+    try {
+      console.log(formData);
+      const { data } = await api.post(`/reviews/${formData?.id}`, {
+        rating: formData?.rating || 0,
+        comment: formData?.comment || "",
+      });
+      // console.log("Backend Responce", data);
+      return data.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.message || error.message);
+    }
+  }
+);
+
+const reviewSlice = createSlice({
+  name: "review",
+  initialState: {
+    reviews: [],
+    loading: false,
+    error: null,
+
+    currPage: 0,
+    isNextPage: false,
+    isPrevPage: false,
+    totalPages: 0,
+    success: false,
+    message: null,
+  },
+  reducers: {
+    removeReviewErrors: (state) => {
+      state.error = null;
+    },
+    removeReviewMessage: (state) => {
+      state.message = null;
+      state.success = false;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProductReviews.pending, (state, action) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getProductReviews.fulfilled, (state, action) => {
+        // console.log("fulfilled action payload: ", action.payload);
+        state.loading = false;
+        state.error = null;
+        state.reviews = action.payload.docs;
+        state.currPage = action.payload.page || 0;
+        state.isNextPage = action.payload.hasNextPage || false;
+        state.isPrevPage = action.payload.hasPrevPage || false;
+        state.totalPages = action.payload.totalPages || 0;
+
+        state.success = false;
+      })
+      .addCase(getProductReviews.rejected, (state, action) => {
+        console.log("Rejected action payload: ", action.payload);
+        state.loading = false;
+        state.error = action.payload || "Something went wrong";
+        state.reviews = [];
+      });
+
+    builder
+      .addCase(createProductReview.pending, (state, action) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createProductReview.fulfilled, (state, action) => {
+        // console.log("fulfilled action payload: ", action.payload);
+        state.loading = false;
+        state.error = null;
+        state.success = true;
+        state.message = action.payload.message;
+      })
+      .addCase(createProductReview.rejected, (state, action) => {
+        // console.log("Rejected action payload: ", action.payload);
+        state.loading = false;
+        state.error = action.payload || "Something went wrong";
+        state.success = false;
+      });
+  },
+});
+
+export const { removeReviewErrors, removeReviewMessage } = reviewSlice.actions;
+export default reviewSlice.reducer;
